7e1e396c276784165d0cbf265688fb25bdf1c342
pwrm18 <127244824+pwrm18@users.noreply.github.com>
2024-01-20 21:12:21 +0300

    more elaborate function

--- main.py
+++ main.py
@@ -2,16 +2,16 @@ user_prompt = "Type 'add', 'show', 'edit', 'complete' or 'exit':"
 with open("todos.txt", 'r') as file:
     todos = file.readlines()
 
-def write_todos():
-    with open('todos.txt', 'w') as file:
-        file.writelines(todos)
+def write_todos(writing, filepath="todos.txt"):
+    with open(filepath, 'w') as file:
+        file.writelines(writing)
 
 while True:
     action = input(user_prompt).strip()
     if action.startswith("add"):
         todo = action[4:]
         todos.append(todo)
-        write_todos()
+        write_todos(todos)
     elif action.startswith("show"):
         for index, todo in enumerate(todos):
             todo = todo.strip("\n")
@@ -20,7 +20,7 @@ while True:
         try:
             number = int(action[9:])
             todos.pop(number - 1)
-            write_todos()
+            write_todos(todos)
         except:
             print("Can't complete that!")
     elif action.startswith("edit"):
@@ -28,7 +28,7 @@ while True:
             number = int(action[5:])
             new_todo = input("Write a new text for a position: ")
             todos[number - 1] = new_todo
-            write_todos()
+            write_todos(todos)
         except:
             print("Your command is not valid! Type a number!")
     elif action.startswith("exit"):
